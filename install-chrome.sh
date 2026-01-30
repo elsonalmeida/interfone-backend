aefee4bb87d4e008ac837b13a14d24f01127cb0c
elsonalmeida <elson.almeida@sti.com.br>
2026-01-30 01:07:50 -0300

    Delete install-chrome.sh

--- install-chrome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "⚙️ Iniciando instalação do Chrome..."
-apt-get update
-apt-get install -y wget unzip
-wget https://storage.googleapis.com/chrome-for-testing-public/144.0.7559.0/linux64/chrome-linux64.zip
-unzip chrome-linux64.zip
-mv chrome-linux64 chrome
-chmod +x chrome/chrome
