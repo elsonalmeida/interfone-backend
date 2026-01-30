e1885b30611aaaa01f76b6811180f799343794cf
elsonalmeida <elson.almeida@sti.com.br>
2026-01-30 00:48:03 -0300

    Update install-chrome.sh

--- install-chrome.sh
+++ install-chrome.sh
@@ -4,3 +4,4 @@ apt-get install -y wget unzip
 wget https://storage.googleapis.com/chrome-for-testing-public/144.0.7559.0/linux64/chrome-linux64.zip
 unzip chrome-linux64.zip
 mv chrome-linux64 chrome
+chmod +x chrome/chrome
